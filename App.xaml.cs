ffd72774c1931bf8314371bdad6af6bfe4702246
Jan-LW <janwichterich@gmx.de>
2026-09-09 10:54:41 +0200

    projekt angelegt

---
+++ App.xaml.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+
+namespace Tetris;
+
+public partial class App : Application
+{
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+using System.Windows;
+using System.Windows.Input;
+
+namespace Tetris;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+        // Hier kommen später die Tasteneingaben rein (Pfeiltasten etc.)
+    }
+}
