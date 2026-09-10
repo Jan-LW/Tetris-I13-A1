bbd3986a3e0b9429e8fab54fde97397388507452
Marlon Frohn <MFrohn@akbk.schule>
2026-09-10 13:32:00 +0200

    Punktsezung Erster Teil

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,15 +3,106 @@ using System.Windows.Input;
 
 namespace Tetris;
 
+public class Tetramino
+{
+    private Point currPosition;
+    private Point[] currShape;
+    private Brush currColor;
+    private bool rotate;
+    public Tetramino()
+    {
+        currPosition = new Point(0, 0);
+        currColor = Brushes.Transparent;
+        currShape = setRandomShape();
+
+    }
+    private Point[] setRandomShape()
+    {
+        Random rand = new Random();
+        switch (rand.Next() % 7)
+        {
+            case 0:
+                rotate = true;
+                currColor = Brushes.Cyan;
+                return new Point[]
+                {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(1,0),
+                        new Point(2,0),
+                };
+            case 1:
+                rotate = true;
+                currColor = Brushes.Blue;
+                return new Point[]
+                {
+                        new Point(1,-1),
+                        new Point(-1,0),
+                        new Point(0,0),
+                        new Point(1,0),
+                };
+            case 2:
+                rotate = true;
+                currColor = Brushes.Orange;
+                return new Point[]
+                {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(1,0),
+                        new Point(1,-1),
+                };
+            case 3:
+                rotate = true;
+                currColor = Brushes.Yellow;
+                return new Point[]
+                {
+                        new Point(0,0),
+                        new Point(0,1),
+                        new Point(1,0),
+                        new Point(1,1),
+                };
+            case 4:
+                rotate = true;
+                currColor = Brushes.Green;
+                return new Point[]
+                {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(0,-1),
+                        new Point(1,0),
+                };
+            case 5:
+                rotate = true;
+                currColor = Brushes.Purple;
+                return new Point[]
+                {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(0,-1),
+                        new Point(1,0),
+                };
+            case 6:
+                rotate = true;
+                currColor = Brushes.Red;
+                return new Point[]
+                {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(0,1),
+                        new Point(1,1),
+                };
+            default:
+                return null;
+        }
+    }
+}
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void Window_KeyDown(object sender, KeyEventArgs e)
-    {
-        // Hier kommen später die Tasteneingaben rein (Pfeiltasten etc.)
     }
 }
